5ba242f9d6334f22d7f850d0557274a74947130b
griffmasters <griffmasters@live.com>
2014-10-23 19:16:11 -0700

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -9,7 +9,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
 
-//recursive int-reversing function
+
 int reverse(int i);
 
 int main()
@@ -19,31 +19,37 @@ int inner = 0;
 int pal = 0;
 int outer = 0;
 int temp;
-    for (outer = 100 ; outer < 1000; ++outer)
+    for (outer = 999 ; outer > 99; outer=outer-1)
     {
-        for(inner = 100 ; inner < 1000; ++inner)
+        for(inner = 999 ; inner > 99; inner=inner-1)
         {
             temp = inner * outer;
-            cout << temp << endl << reverse(temp) << endl << endl; 
+//           cout << temp << endl << reverse(temp) << endl << endl; 
             if(temp == reverse(temp) && temp > pal)
-//                cout << inner << endl << outer << endl << endl;
-                pal=temp;
+                {
+                    cout << inner << endl << outer << endl << endl;
+                    pal=temp;
+                }
         }
     }
     
-   cout << pal << endl << outer << endl << inner << endl; 
+   cout << pal << endl; 
    
    return 0;
 }
 
 
-//recursive int-reversing function
+// int-reversing function
 int reverse(int i)
 {
+    int rev = 0;
+    
+    while(i>0)
+    {
+        rev = rev * 10;
+        rev = rev + i%10;
+        i=i/10;
+    }
 
-    if(i<10)
-        return i;
-        
-    else
-        return 10*((i%10) + reverse(i/10));
+    return rev;
 }
